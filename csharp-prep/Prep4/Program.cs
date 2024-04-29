bee23c44f57fcee8476266b8ebc363678c364633
JLKohl <121449936+JLKohl@users.noreply.github.com>
2024-04-29 16:01:25 -0400

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,32 @@
 using System;
-
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do{
+            Console.Write("Enter number: ");
+
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+
+        }while(userNumber != 0);
+
+        
+        Console.WriteLine($"the sum is {numbers.Sum()} ");
+        Console.WriteLine($"the sum is {numbers.Average()} ");
+        Console.WriteLine($"the sum is {numbers.Max()} ");
+
     }
 }
