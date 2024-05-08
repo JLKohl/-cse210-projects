95e02d1d675b9366f38a59b7490432064e48942e
JLKohl <121449936+JLKohl@users.noreply.github.com>
2024-05-08 11:20:53 -0400

    job class finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+       Job job1 = new Job();
+
+       job1._jobTitle = "Software Engineer";
+       job1._company = "Disney";
+       job1._startYear = 2040;
+       job1._endYear = 2100;
+
+       Job job2 = new Job();
+
+       job2._jobTitle = "Mom";
+       job2._company = "Kohl House";
+       job2._startYear = 2010;
+       job2._endYear = 3000;
+
+       job1.Display();
+       job2.Display();
+;
     }
 }
