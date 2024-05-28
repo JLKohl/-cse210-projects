87d33b24885ba99ce9f2c684bc0af3e69061a199
JLKohl <121449936+JLKohl@users.noreply.github.com>
2024-05-28 15:14:29 -0400

    refreance class built

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,43 @@
+
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _startVerse;
+    private int _endVerse;
+
+    public Reference()
+    {
+        _book = "unknown";
+        _chapter = 0;
+        _startVerse = 0;
+        _endVerse = 0;
+    }
+
+    public Reference(string book, int chapter, int startVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = startVerse;
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = startVerse;
+        _endVerse = endVerse;
+    }
+
+    public string GetRefrerence()
+    {
+        string text = $"{_book} {_chapter}:{_startVerse}";
+        return text;
+    }
+
+    public string GetRefrerenceRange()
+    {
+        string text = $"{_book} {_chapter}:{_startVerse}-{_endVerse}";
+        return text;
+    }
+}
