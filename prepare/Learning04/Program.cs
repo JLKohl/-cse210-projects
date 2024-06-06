872beeae0ee86f714f10a7c21f34f63724852139
JLKohl <121449936+JLKohl@users.noreply.github.com>
2024-06-06 15:04:30 -0400

    finished program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment one = new Assignment("Jennifer Kohl", "Art History");
+
+        Console.WriteLine(one.GetSummery());
+
+        MathAssignment two = new MathAssignment("Jason Kohl", "Cool People", "7.5", "10-35");
+        Console.WriteLine(two.GetSummery());
+        Console.WriteLine(two.GetHomeworkList());
+
+        WritingAssigment three = new WritingAssigment("Joseph Kohl", "getting buff", "I am the strongest kid");
+        Console.WriteLine(three.GetSummery());
+        Console.WriteLine(three.GetWritingInformation());
     }
 }
