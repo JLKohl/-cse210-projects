0ebe0d90beb430b06ad55e7f41f3da0fcb2bf029
JLKohl <121449936+JLKohl@users.noreply.github.com>
2024-06-19 12:08:41 -0500

    finished poloymorphisum leanring

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square square = new Square("blue", 10);
+        shapes.Add(square);
+        
+        Rectangle rectangle = new Rectangle("purple", 6, 9);
+        shapes.Add(rectangle);
+        
+        Circle circle = new Circle("red", 8);
+        shapes.Add(circle);
+        
+        
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+        {
+            
+        }
     }
 }
